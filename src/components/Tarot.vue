4f0d5c303dc972f643c4716fd8b1a6f22900e592
TagYut <Tagami.Yutaro@gmail.com>
2018-04-24 09:51:22 +0900

    canvasで遊んでいる

--- src/components/Tarot.vue
+++ src/components/Tarot.vue
@@ -1,10 +1,13 @@
 <template>
-  <div class="Tarot">
-    <h1 class="Tarot__title">tarot</h1>
-    <div class="Tarot__box">
-      <div class="Tarot__view"></div>
-      <div class="Tarot__input">
-        <p>Please Input Your Birthday</p>
+  <div class="tarot">
+    <h1 class="tarot__title">tarot</h1>
+    <div class="tarot__box">
+      <div class="tarot__view">
+        <canvas id="canvas" @click="canvasBall">ああああ</canvas>
+      </div>
+      <div class="tarot__input">
+        <p>{{ randam }} をつかって乱数をつくるぞ</p>
+        <input v-model="randam" placeholder="ここになんかかきこめ">
       </div>
     </div>
   </div>
@@ -15,7 +18,22 @@ export default {
   name: 'HelloWorld',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: 'Welcome to Your Vue.js App',
+      randam: 'こいつ'
+    }
+  },
+  methods: {
+    canvasBall: function () {
+      var canvas = document.getElementById('canvas')
+      if (canvas.getContext) {
+        var context = canvas.getContext('2d')
+        context.fillRect(20, 40, 50, 100)
+        context.strokeStyle = 'rgb(00,00,255)'
+        context.fillStyle = 'rgb(255,00,00)'
+        context.strokeRect(200, 80, 100, 50)
+        context.arc(150, 75, 60, Math.PI * 1, Math.PI * 2, true)
+        context.fill()
+      }
     }
   }
 }
@@ -26,24 +44,28 @@ export default {
 * {
   box-sizing: border-box;
 }
-.Tarot {
+canvas {
+  height: 300px;
+  width: 600px;
+}
+.tarot {
   &__title {
     border: 2px solid black;
     border-bottom-width: 0;
     margin: 0 auto;
-    width: 600px;
+    width: 604px;
   }
   &__box {
     border: 2px solid black;
     margin: 0 auto;
     height: 400px;
-    width: 600px;
+    width: 604px;
   }
   &__view {
     border-bottom: 2px solid black;
     margin: 0 auto;
     height: 300px;
-    width: 598px;
+    width: 600px;
   }
   &__input {
     height: 100px;
