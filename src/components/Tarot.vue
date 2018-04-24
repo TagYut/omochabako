f46f19510d9e974052ea483ea3a11417ac6b2cee
TagYut <Tagami.Yutaro@gmail.com>
2018-04-24 11:58:23 +0900

    背景黒くした

--- src/components/Tarot.vue
+++ src/components/Tarot.vue
@@ -28,7 +28,7 @@ export default {
       var gl = canvas.getContext('webgl')
       if (canvas.getContext) {
         gl.viewport(0, 0, canvas.width, canvas.height)
-        gl.clearColor(1, 0, 0, 1)
+        gl.clearColor(0, 0, 0, 1)
         gl.clear(gl.COLOR_BUFFER_BIT)
       }
     }
